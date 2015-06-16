d0a0abdfe9841e4ac3979d4bceaf835cd7603824
Shirren Premaratne <shirren.premaratne@gmail.com>
2015-06-16 17:47:17 +1000

    Implemented the average function for durations

--- lib/continuance/durations.rb
+++ lib/continuance/durations.rb
@@ -9,17 +9,29 @@ module Continuance
       @items = items
     end
 
-    # Given a series of durations, this function calculates the average duration.
+    # Given a series of durations, this function calculates the average duration
+    # by converting each duration to an absolute float value
     def average
-      total = items.inject(0.0) do |sum, duration|
+      time = Time.at(avg_as_f).utc.strftime("%H:%M:%S:%N")
+      Duration.create(time, '%H:%M:%S:%L')
+    end
+
+    # Provides a sum of all the durations
+    def total
+      items.inject(0.0) do |sum, duration|
         sum += duration.to_f if duration.respond_to?(:to_f)
       end
+    end
+
+    private
+
+    # Calculates the average of the time as a float
+    def avg_as_f
       if items.empty?
-        avg = 0.0
+        0.0
       else
-        avg = total / items.size
+        total / items.size
       end
-      Duration.create(avg.to_s, '%S')
     end
   end
 end
